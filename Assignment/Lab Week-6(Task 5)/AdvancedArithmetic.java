ae436e0cf4247d8cf166b5c8c124531740c5ead6
sajjad-njr <maxsajjad29@gmail.com>
2021-10-12 00:30:47 +0600

    Task 05

---
+++ Assignment/Lab Week-6(Task 5)/AdvancedArithmetic.java
@@ -0,0 +1,18 @@
+package sajjad;
+
+import java.math.BigInteger;
+
+public interface AdvancedArithmetic {
+    /*
+        Md. Sajjadur Rahman
+        2012020299
+        5F
+        cse_2012020299@lus.ac.bd
+       Date : 10 - 10 - 2021
+
+     */
+
+    int divisorSum(int n) ;
+
+    BigInteger findFactorial(int n);
+}
---
+++ Assignment/Lab Week-6(Task 5)/Main.java
@@ -0,0 +1,22 @@
+package sajjad;
+
+import sajjad.MyCalculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        Md. Sajjadur Rahman
+        2012020299
+        5F
+        cse_2012020299@lus.ac.bd
+       Date : 10 - 10 - 2021
+        */
+        MyCalculator obj = new MyCalculator();
+
+        System.out.println(obj.divisorSum(50));
+        System.out.println(obj.findFactorial(40));
+
+    }
+}
